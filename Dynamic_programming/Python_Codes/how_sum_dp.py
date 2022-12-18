12ddafdcf94e739411d511ee594e96e92eb29f62
Yang Fei <48932401+YangFei9606@users.noreply.github.com>
2022-12-18 15:13:57 +0800

    Add files via upload

--- Dynamic_programming/Python_Codes/how_sum_dp.py
+++ Dynamic_programming/Python_Codes/how_sum_dp.py
@@ -3,6 +3,7 @@ import array
 def how_sum_dp(target, choices, dictionary_how):
     if target == 0:
         # If target is 0, we return [] to represent that we can start saving answers
+        dictionary_how[0] = []
         return [], dictionary_how
     elif target < 0:
         # If target is negative, we return None to represent that we do not save this path
@@ -24,11 +25,13 @@ def how_sum_dp(target, choices, dictionary_how):
         dictionary_how[target] = temp_reu
         return dictionary_how[target], dictionary_how
 
-target = 40
-choices = [6, 14]
+target = 7
+choices = [2, 3]
 dictionary_how = {}
 
 result, dictionary_how = how_sum_dp(target, choices, dictionary_how)
 
 print(dictionary_how.get(target))
 print(dictionary_how)
+
+

0dec9ebc2ad3ba21e64e7353d2b378f28bba0d4e
Yang Fei <48932401+YangFei9606@users.noreply.github.com>
2022-12-27 16:58:15 +0800

    Update fibonacci_tabu.py

--- Dynamic_programming/Python_Codes/fibonacci_tabu.py
+++ Dynamic_programming/Python_Codes/fibonacci_tabu.py
@@ -1,7 +1,7 @@
 def fib_tabu(num):
 
     list_all = [0] * (num + 1)
-    if num >= 2:
+    if num >= 1:
         list_all[1] = 1
         for i in range(2, num + 1):
             list_all[i] = list_all[i-1] + list_all[i-2]

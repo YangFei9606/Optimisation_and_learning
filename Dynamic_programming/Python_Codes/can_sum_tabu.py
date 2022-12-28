ab9f06dda992f7490849f00f692b0ad184329856
Yang Fei <48932401+YangFei9606@users.noreply.github.com>
2022-12-28 16:23:32 +0800

    Update can_sum_tabu.py

--- Dynamic_programming/Python_Codes/can_sum_tabu.py
+++ Dynamic_programming/Python_Codes/can_sum_tabu.py
@@ -5,10 +5,11 @@ def can_sum_tabu(target, numbers):
     for i in range(0, target + 1):
         if list_can[i] is True:
             for j in numbers:
-                list_can[j] = True
+                if i+j <= target:
+                    list_can[i+j] = True
 
     return list_can[-1]
 
-target = 30000
+target = 70001
 numbers = [7, 14]
 print(can_sum_tabu(target, numbers))

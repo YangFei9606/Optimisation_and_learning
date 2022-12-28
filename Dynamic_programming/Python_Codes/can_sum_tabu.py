e96d7df474e832fa512f2825d8427ca2ec82e51b
Yang Fei <48932401+YangFei9606@users.noreply.github.com>
2022-12-28 12:00:22 +0800

    Create can_sum_tabu.py

---
+++ Dynamic_programming/Python_Codes/can_sum_tabu.py
@@ -0,0 +1,14 @@
+def can_sum_tabu(target, numbers):
+    list_can = [False] * (target + 1)
+    list_can[0] = True
+
+    for i in range(0, target + 1):
+        if list_can[i] is True:
+            for j in numbers:
+                list_can[j] = True
+
+    return list_can[-1]
+
+target = 30000
+numbers = [7, 14]
+print(can_sum_tabu(target, numbers))

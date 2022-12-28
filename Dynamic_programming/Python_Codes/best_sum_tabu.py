68d99c364066cd18164f376a73f026e61ea2d74d
Yang Fei <48932401+YangFei9606@users.noreply.github.com>
2022-12-28 21:45:48 +0800

    Update best_sum_tabu.py

--- Dynamic_programming/Python_Codes/best_sum_tabu.py
+++ Dynamic_programming/Python_Codes/best_sum_tabu.py
@@ -6,11 +6,11 @@ def best_sum_tabu(target, numbers):
         if list_best[i] is not None:
             for j in numbers:
                 if i + j <= target:
-                    if (list_best[i + j] is None) or ( (list_best[i + j] is not None) and (len(list_best[i + j]) > len(list_best[i]) + 1 ) ):
+                    if (list_best[i + j] is None) or (len(list_best[i + j]) > len(list_best[i]) + 1 ) :
                         list_best[i + j] = list_best[i] + [j]
 
     return list_best[-1]
 
-target = 200
+target = 69
 numbers = [2, 50, 10]
 print(best_sum_tabu(target, numbers))

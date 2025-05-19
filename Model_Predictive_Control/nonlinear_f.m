3acf1a7dc73916dc0bfb3aeffb72bbb90dcd0662
Yang Fei <48932401+YangFei9606@users.noreply.github.com>
2025-05-19 16:25:22 +0800

    Delete Model_Predictive_Control directory

--- Model_Predictive_Control/nonlinear_f.m
+++
@@ -1,5 +0,0 @@
-function dx = nonlinear_f(x, u)
-    % 2维状态2维输入示例
-    dx = [-x(1) + sin(u(1));
-          -x(2) + cos(u(2))];
-end
--- Model_Predictive_Control/nonlinear_h.m
+++
@@ -1,3 +0,0 @@
-function y = nonlinear_h(x)
-    y = x; % 直接输出状态
-end
